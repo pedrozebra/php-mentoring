f242c977a09d0931c3513905286b13f1352fd3b9
pedrozebra <pdellanotte@gmail.com>
2022-06-22 23:03:47 +0200

    added test

---
+++ tests/ProductTest.php
@@ -0,0 +1,28 @@
+<?php
+
+use PhpMentoring\Classes\Product;
+use PhpMentoring\Classes\Category;
+use PHPUnit\Framework\TestCase;
+
+class ProductTest extends TestCase
+{
+    public function testClassConstructor()
+    {
+        $category = new Category('1', 'Jewerly');
+        $product = new Product('1', 'BVLGARI Serpenti', $category, 'Last luxury BVLGARI creation', '10000$', true);
+        self::assertSame('BVLGARI Serpenti', $product->name);
+        self::assertSame('Last luxury BVLGARI creation', $product->description);
+        self::assertSame('10000$', $product->price);
+        self::assertSame(true, $product->isOnline);
+        self::assertSame('Jewerly', $product->category->name);
+        self::assertIsBool($product->isOnline);
+    }
+
+    public function testGetName()
+    {
+        $category = new Category('1', 'Jewerly');
+        $product = new Product('1', 'BVLGARI Serpenti', $category, 'Last luxury BVLGARI creation', '10000$', true);
+        $this->assertIsString($product->__get('name'));
+        $this->assertStringContainsStringIgnoringCase('BVLGARI Serpenti', $product->__get('name'));
+    }
+}
