3ddd2cb7fd7ac026b8b324cf80433ed11f5ca19f
pedrozebra <pdellanotte@gmail.com>
2022-06-23 11:35:00 +0200

    modified index

--- public/index.php
+++ public/index.php
@@ -2,22 +2,24 @@
 
 declare(strict_types=1);
 
-ini_set('display_errors', '1');
-error_reporting(E_ALL);
-
 require __DIR__ . '/../src/Bootstrap.php';
 
-
+use PharIo\Manifest\Email;
 use PhpMentoring\Classes\Category;
 use PhpMentoring\Classes\Product;
+use PhpMentoring\classes\User;
 
-$category = new Category('1211', 'Test Category');
-$category2 = new Category('12112', 'Test Category2');
+$category = new Category('1211', 'Jewerly');
+$categoryTwo = new Category('1211', 'Watches');
+$user = new User('7374hryU', "pedrozebra", new Email("pdellanotte@gmail.com"));
 
-$product = new Product('1111','BVLGARI Serpenti Ring', $category, 'Last luxury BVLGARI creation', '15000$', true);
+$product = new Product('1111', 'BVLGARI Serpenti Ring', $category, 'Last luxury BVLGARI creation', '15000$', true);
+$productTwo = new Product('356910', 'BVLGARI Save the Children Necklace', $category, 'Save the Children 10th anniversary necklace in sterling silver with pendant set with onyx element and a ruby', '770$', true);
+$productThree = new Product('103611', 'BVLGARI Save the Children Necklace', $categoryTwo, 'Octo Finissimo Ultra watch with mechanical manufacture ultra-thin movement, manual winding, sandblasted titanium case', '45000$', true);
+$user->addFavoriteProduct($product);
+$user->addFavoriteProduct($productTwo);
+$user->addFavoriteProduct($productThree);
 
-echo($product);
-
-$product->name = 'test';
-$product->category = $category2;
-echo($product);
+echo ($product);
+echo "<br/><br/><br/>";
+echo ($user);
